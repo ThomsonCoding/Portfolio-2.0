39faf2d72ea1c1641a9b68b726a7aac6daebbdf1
Daniel Thomson <67236266+ThomsonCoding@users.noreply.github.com>
2023-01-22 21:01:28 +0000

    Navbar mobile development

--- js/navigation.js
+++ js/navigation.js
@@ -1,14 +1,9 @@
 const body = document.querySelector('body');
 const modalOpenButton = document.querySelector('.navigation__open');
-const modalCloseButton = document.querySelector('.navigation__close');
 const navigationMeuu = document.querySelector('.navigation');
 const overlay = document.querySelector('.overlay');
 
 modalOpenButton.addEventListener('click', () => {
-    toggleModal();
-});
-
-modalCloseButton.addEventListener('click', () => {
     toggleModal();
 });
 
