7bda233a9c74edfc731193cd7e3d7d7fdc04f576
Daniel Thomson <67236266+ThomsonCoding@users.noreply.github.com>
2023-03-14 20:05:13 +0100

    Code change

--- js/deviceSize.js
+++ js/deviceSize.js
@@ -1,4 +1,9 @@
-window.onresize = function() {
-    document.body.height = window.innerHeight;
+const resizeBody = () => {
+    document.body.style.height = window.innerHeight + "px";
 }
-window.onresize(); 
+
+  // Call the function when the page loads
+window.onload = resizeBody;
+
+  // Call the function again whenever the window is resized
+window.onresize = resizeBody;
