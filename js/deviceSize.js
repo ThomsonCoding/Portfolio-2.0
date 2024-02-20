8477e0e94e7fd938b18795e79254b084bef151c5
Daniel Thomson <67236266+ThomsonCoding@users.noreply.github.com>
2024-02-20 10:53:55 +0000

    removal of deviceSize js

--- js/deviceSize.js
+++ js/deviceSize.js
@@ -1,10 +1,10 @@
-const resizeBody = () => {
-    const page = document.querySelector('.page');
-    page.style.height = window.innerHeight + "px";
-}
+// const resizeBody = () => {
+//     const page = document.querySelector('.page');
+//     page.style.height = window.innerHeight + "px";
+// }
 
-  // Call the function when the page loads
-window.onload = resizeBody;
+//   // Call the function when the page loads
+// window.onload = resizeBody;
 
-  // Call the function again whenever the window is resized
-window.onresize = resizeBody;
+//   // Call the function again whenever the window is resized
+// window.onresize = resizeBody;
