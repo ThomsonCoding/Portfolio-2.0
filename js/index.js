a8239e0c630b839655ce68692f1ed0074e9f0c9f
Daniel Thomson <67236266+ThomsonCoding@users.noreply.github.com>
2024-03-03 23:00:25 +0000

    renaming and changes

--- js/index.js
+++ js/index.js
@@ -2,5 +2,4 @@ import '../js/deviceSize.js';
 import '../js/navigation.js';
 import '../js/dataLayer.js';
 import '../js/misc.js';
-import '../js/ourProcessToggle.js';
-import '../js/faqSearchbox.js';
+
