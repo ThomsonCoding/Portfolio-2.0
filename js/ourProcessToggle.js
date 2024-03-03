b608fbd4b9a0b2c14bb1510aff37b5860a747a1e
Daniel Thomson <67236266+ThomsonCoding@users.noreply.github.com>
2024-03-03 22:46:05 +0000

    sliders per view tweak

--- js/ourProcessToggle.js
+++ js/ourProcessToggle.js
@@ -25,10 +25,10 @@
     },
     breakpoints: {
         560: {
-        slidesPerView: 2.5,
+        slidesPerView: 2,
         },
         768: {
-        slidesPerView: 3,
+        slidesPerView: 2,
         },
         1024: {
         slidesPerView: 3,
