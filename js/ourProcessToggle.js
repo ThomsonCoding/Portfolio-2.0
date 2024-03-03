5cf99a11341be025b578e72289641420f7673ec0
ThomsonCoding <thomsoncoding@gmail.com>
2024-03-03 15:45:48 +0000

    restsrt our process

--- js/ourProcessToggle.js
+++ js/ourProcessToggle.js
@@ -1,10 +1,1 @@
-const ourProcessSection = document.querySelector('.our-process__section');
-const ourProcessToggleButtons = document.querySelectorAll('.toggle_radio input');
-
-ourProcessToggleButtons.forEach((button) => {
-    button.addEventListener('click',  () => toggleButton(button));
-})
-
-function toggleButton(button) {
-    ourProcessSection.classList = `our-process__section ${button.id}`; 
-}
+import Swiper, { Navigation, Pagination } from 'swiper';
