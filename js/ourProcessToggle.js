44c3722567c40116dfd5e98d454047f6b6f387e7
Daniel Thomson <67236266+ThomsonCoding@users.noreply.github.com>
2024-03-03 16:13:46 +0000

    updates

--- js/ourProcessToggle.js
+++ js/ourProcessToggle.js
@@ -1,1 +1,38 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
+import 'swiper/swiper-bundle.css'; // Import Swiper styles
+import Swiper from 'swiper/bundle'; 
+
+    let swiper = new Swiper(".swiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 4,
+        slideShadows: true,
+    },
+    loop: true,
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    keyboard: {
+        enabled: true,
+    },
+    mousewheel: {
+        thresholdDelta: 70,
+    },
+    breakpoints: {
+        560: {
+        slidesPerView: 2.5,
+        },
+        768: {
+        slidesPerView: 3,
+        },
+        1024: {
+        slidesPerView: 3,
+        },
+    },
+    });
